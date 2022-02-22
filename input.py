5d4c26f939c55d4f9fc9472b4615e2797728c4ba
KiligJason <93173133+KiligJason@users.noreply.github.com>
2022-02-22 11:18:09 +0800

    Add files via upload

---
+++ input.py
@@ -0,0 +1,49 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+# Ziyiyang Wang 1098529
+# Zhaoze Wang 1098471
+
+my_name = str(input())
+Jason
+type(my_name)
+<class 'str'>
+my_age = int(input())
+22
+type(my_age)
+<class 'int'>
+my_street = str(input())
+Wenzhou
+type(my_street)
+<class 'str'>
+my_sport = str(input())
+basketball
+type(my_sport)
+<class 'str'>
+#result
+print("Hello,my name is :"+my_name+'\n'+
+      "I am "+str(my_age)+" years\n"+
+      "I live in "+my_street+
+      "\nI like to play "+my_sport)
+Hello,my name is :Jason
+I am 22 years
+I live in Wenzhou
+I like to play basketball
+
+
+>>> my_name = str(input())
+Irving
+>>> my_age = int(input())
+22
+>>> my_street = str(input())
+Wenzhou
+>>> my_sport = str(input())
+Baseball
+#result
+print("Hello, my name is :" + my_name +'\n' +
+      "I am " + str(my_age) +" years\n" +
+      "I live in " + my_street + '\n'
+      "I like to play " + my_sport)
+Hello, my name is :Irving
+I am 22 years
+I live in Wenzhou
+I like to play Baseball
